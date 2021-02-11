43ed23c5be1d784c0b1108cf3ba260cc99912f43
Matthew Feickert <matthew.feickert@cern.ch>
2021-02-11 01:33:10 -0600

    Lower tolerance for no_grad and 32b

--- tests/test_optim.py
+++ tests/test_optim.py
@@ -109,7 +109,7 @@ def test_minimize(tensorlib, precision, optimizer, do_grad, do_stitch):
         abs_tol = 1e-6 if "32b" in identifier else 1e-8
 
         # handle cases where macos and ubuntu provide very different results numerical
-        if "no_grad" in identifier:
+        if "no_grad" or "32b" in identifier:
             rel_tol = 1e-5
             if "minuit-pytorch-32b" in identifier:
                 # quite a large difference between local and CI
@@ -119,8 +119,6 @@ def test_minimize(tensorlib, precision, optimizer, do_grad, do_stitch):
                 rel_tol = 3e-2
             if "minuit-jax-32b" in identifier:
                 rel_tol = 4e-2
-        if "do_grad-scipy-jax-32b" in identifier:
-            rel_tol = 1e-5
 
         # check fitted parameters
         assert pytest.approx(
